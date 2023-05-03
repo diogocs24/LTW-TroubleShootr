7d1abdf74d650a36a3a523b91145fcdbc2c8b5e9
Guilherme <guilherme.cpos1904@gmail.com>
2023-05-03 17:12:59 +0100

    .p

--- por_diretorios_depois/drawcommon.php
+++ por_diretorios_depois/drawcommon.php
@@ -49,8 +49,6 @@
 					</ul>
 				</div>
 			</footer>
-			</body>
-            </html>
  <?php } ?>
 
  <?php function draw_nav2() {?>
@@ -79,6 +77,95 @@
 
 
 
+<?php function draw_pagecontainer(){?>
+    <div id="page-container">
+    <main id="main">
+        <div class="content">
+            <h1 data-text="Troubleshoot with us!">Troubleshoot with us!</h1>
+            <p>Get efficient and effective 
+                support from our team and solve any problem, no matter how big or small. Join us now and 
+                let's troubleshoot together!</p>
+        </div>
+        <div class="wrapper">
+            <span class="icon-close"
+                ><ion-icon name="close"></ion-icon
+            ></span>
+            <div class="form-box login">
+                <h2>Login</h2>
+                <form action="#">
+                    <div class="input-box">
+                        <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                        <!--Change the type to email-->
+                        <input type="text" required/>
+                        <label>Email</label>
+                    </div>
+                    <div class="input-box">
+                        <span class="icon"><ion-icon name="lock-closed"></span>
+                        <input type="password" required>
+                        <label>Password</label>
+                    </div>
+                    <div class="remember-forgot">
+                        <label><input type="checkbox">Remember me</label>
+                        <a href="#">Forgot Password</a>
+                    </div>
+                    <button type="submit" class="btn">Login</button>
+                    <div class="login-register">
+                        <p>Don't have an account? <a href="#" class="register-link">Register</a></p>
+                    </div>
+                </form>
+            </div>
+            <div class="form-box register">
+                <h2>Registration</h2>
+                <form action="#">
+                    <div class="input-box">
+                        <span class="icon"><ion-icon name="person"></ion-icon></span>
+                        <input type="text" required />
+                        <label>Username</label>
+                    </div>
+                    <div class="input-box">
+                        <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                        <!--Change the type to email-->
+                        <input type="email" required />
+                        <label>Email</label>
+                    </div>
+                    <div class="input-box">
+                        <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                        <input type="password" required />
+                        <label>Password</label>
+                    </div>
+                    <div class="remember-forgot">
+                        <label
+                            ><input type="checkbox" />I agree to the terms & conditions</label
+                        >
+                    </div>
+                    <button type="submit" class="btn">Register</button>
+                    <div class="login-register">
+                        <p>
+                            Already have an account?
+                            <a href="#" class="login-link">Login</a>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+</div>
+<?php}?>
+
+<?php function draw_script(){ ?>
+	<script src="script_dark-theme.js"></script>
+		<script src="script.js"></script>
+		<script
+			type="module"
+			src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+		></script>
+		<script
+			nomodule
+			src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+		></script>
+	</body>
+</html>
+<?php } ?>
 
 
 
--- por_diretorios_depois/home_page1.php
+++ por_diretorios_depois/home_page1.php
@@ -3,6 +3,6 @@ require_once(__DIR__.'/drawcommon.php');
 drawHeader();
 draw_nav1();
 draw_pagecontainer();
-draw_script();
 drawFooter();
+draw_script();
 ?>
