42f2b10a118901eaaa0a4a7503e0171a34afb21b
diogocs18 <diogopinguinhas@hotmail.com>
2023-05-17 18:52:39 +0100

    fixes

--- a/tickets.php
+++ a/tickets.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 require_once(__DIR__.'/../a/pages_draw.php');
 require_once(__DIR__.'/../database/config.php');
 require_once(__DIR__.'/../database/ticket.php');
-require once(__DIR__.'/../a/session.php');
+require_once(__DIR__.'/../a/session.php');
 
 $db = getDatabaseConnection();
 
